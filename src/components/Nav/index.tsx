a76d00f5b55d6e8c86f8f997dba8427c9dd4ab37
Kranti <darkphoton20@gmail.com>
2024-07-20 03:33:31 +0800

    Added tailwind and adjusted global css.

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,14 +1,17 @@
 import { navData } from "@/data/nav";
-import styles from "./styles.module.css";
 import Link from "next/link";
 
 const Nav = () => {
 	return (
 		<>
-			<nav className={styles.nav}>
-				<ul>
+			<nav className="p-3">
+				<ul className="flex gap-3">
 					{navData.map((navItem) => (
-						<li className={styles.navItem}>
+						<li
+							key={navItem.link}
+							className="-gray-900 transition-colors duration-200 ease-in
+							hover:text-gray-500 cursor-pointer"
+						>
 							<Link href={navItem.link}>{navItem.name}</Link>
 						</li>
 					))}
